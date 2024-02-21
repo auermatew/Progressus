cda31132c16a95ed20c56235339067d2c51f3eb2
Preisler25 <preisler.andras@gmail.com>
2024-02-21 10:45:02 +0100

    type: refact
    refactoring app and db

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-import express from 'express';
-import DbService from './service/db_service';
-import dotenv from 'dotenv';
+import App from "./app";
 
-class App{
-
-    public app: express.Application;
-    public dbService: DbService;
-
-    constructor(){
-        dotenv.config();
-        this.app = express();
-        this.dbService = new DbService();
-    }
-
-    public listen(){
-        this.app.listen(3000, () => {
-            console.log('Server is running on port 3000');
-        });
-    }
-
-}
+const app = new App();
